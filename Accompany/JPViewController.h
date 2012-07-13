3f903b51c607f893aca76aa9f067265e5385899b
jacobpan3g <zhenjian3g@qq.com>
2012-07-13 21:59:22 +0800

    TableView non-success

--- Accompany/JPViewController.h
+++ Accompany/JPViewController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>ß
+#import <UIKit/UIKit.h>
 
 @interface JPViewController : UIViewController
 
---
+++ Accompany/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  Accompany
+//
+//  Created by Pan Jacob on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController //<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
+@end
